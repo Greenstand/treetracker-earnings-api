6563b9b99f92f37efb7369851e3a660225ff230a
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2021-10-23 01:55:34 +0100

    fix: add earnings schema to batch repository

--- server/repositories/BatchRepository.js
+++ server/repositories/BatchRepository.js
@@ -2,8 +2,8 @@ const BaseRepository = require('./BaseRepository');
 
 class BatchRepository extends BaseRepository {
   constructor(session) {
-    super('batch', session);
-    this._tableName = 'batch';
+    super('earnings.batch', session);
+    this._tableName = 'earnings.batch';
     this._session = session;
   }
 }
