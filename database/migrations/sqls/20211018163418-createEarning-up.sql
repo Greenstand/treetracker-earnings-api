71906abbd1753c9628d5cbe00d5e5c9665447e03
Zaven Arra <matthew.fullstackgis@gmail.com>
2021-10-29 10:10:24 -0700

    fix: open up CORS for dev

--- database/migrations/sqls/20211018163418-createEarning-up.sql
+++ database/migrations/sqls/20211018163418-createEarning-up.sql
@@ -5,10 +5,10 @@ CREATE TYPE currency_enum AS ENUM ('USD');
 
 CREATE TABLE earnings
 (
-    id uuid NOT NULL PRIMARY KEY,
-    worker_id uuid NOT NULL REFERENCES stakeholder.stakeholder(id),
-    contract_id uuid NOT NULL REFERENCES contract.contract(id),
-    funder_id uuid NOT NULL REFERENCES stakeholder.stakeholder(id),
+    id uuid NOT NULL DEFAULT uuid_generate_v4() PRIMARY KEY,
+    worker_id uuid NOT NULL, 
+    contract_id uuid NOT NULL,
+    funder_id uuid NOT NULL,
     amount numeric NOT NULL,
     currency currency_enum NOT NULL,
     calculated_at timestamptz NOT NULL,
@@ -18,10 +18,10 @@ CREATE TABLE earnings
     payment_confirmation_id varchar,
     payment_system varchar,
     payment_confirmed_by uuid,
-    payment_confirmation_method confirmation_method_enum NOT NULL,
+    payment_confirmation_method confirmation_method_enum,
     paid_at timestamptz,
     payment_confirmed_at timestamptz,
     status earning_status_enum NOT NULL,
-    active boolean NOT NULL,
+    active boolean DEFAULT true NOT NULL,
     batch_id uuid REFERENCES batch(id)
 );
