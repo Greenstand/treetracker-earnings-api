41479e6e0ab4692d95479ea8e462e594d413e158
Zaven Arra <matthew.fullstackgis@gmail.com>
2021-11-10 18:13:45 -0800

    fix: remove schema creationg from migrations

--- database/migrations/sqls/20211010192212-createBatch-up.sql
+++ database/migrations/sqls/20211010192212-createBatch-up.sql
@@ -1,9 +1,8 @@
 CREATE TYPE status AS ENUM ('created', 'completed', 'failed');
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE SCHEMA IF NOT EXISTS earnings;
 
-CREATE TABLE IF NOT EXISTS earnings.batch (
+CREATE TABLE IF NOT EXISTS batch (
     id uuid NOT NULL PRIMARY KEY DEFAULT uuid_generate_v4(),
     url varchar NOT NULL,
     status status NOT NULL,
