50fee1d7a79555f694360c3024b12e345b4e8c9a
Daniel Olojakpoke <danielolojakpoke@gmail.com>
2021-11-16 03:03:19 +0100

    chore: update db schema

--- database/migrations/sqls/20211027043106-alterTableName-up.sql
+++ database/migrations/sqls/20211027043106-alterTableName-up.sql
@@ -1,2 +1,2 @@
 /* Replace with your SQL commands */
-ALTER TABLE earnings.earnings RENAME COLUMN consolidation_id TO consolidation_rule_id;
+ALTER TABLE earnings RENAME COLUMN consolidation_id TO consolidation_rule_id;
