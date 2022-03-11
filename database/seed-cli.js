f4d6b0f455e6d4ded82ee7aabb9dd8f29516531c
deanchen <antlet2004@gmail.com>
2022-03-11 11:39:33 +0800

    feat: commander

--- database/seed-cli.js
+++ database/seed-cli.js
@@ -1,9 +1,28 @@
-const seed = require('./seed/index');
 const log = require("loglevel");
+const seed = require('./seed/index');
+const { Command } = require('commander');
+const program = new Command();
 
-log.warn("seeding...");
-seed.seed()
-  .then(() => {
-    process.exit(0);
-  })
-log.warn("done");
+program
+  .name('seed-cli')
+  .description('CLI to seed data into DB for testing')
+  .version('0.1.0');
+
+program.command('earnings')
+  .description('Seed an earings record into earning db')
+  // .argument('<string>', 'string to split')
+  .requiredOption('-f, --funder_id <string>', 'set up the funder id')
+  .requiredOption('-g, --grower_id <string>', 'set up the grower id')
+  .requiredOption('-c, --captures_count', 'the number of captures')
+  .action((options) => {
+    // const limit = options.first ? 1 : undefined;
+    // console.log(str.split(options.separator, limit));
+    log.warn("seeding...", options);
+    // seed.seed()
+    //   .then(() => {
+    //     process.exit(0);
+    //   })
+    log.warn("done");
+  });
+
+program.parse();
