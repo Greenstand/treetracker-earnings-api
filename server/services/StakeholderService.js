979ea756a054584c8785a9ce0ec622378c4934fe
Olojakpoke Daniel <holodja@hotmail.com>
2023-07-18 01:22:10 +0100

    fix: stakeholder url

--- server/services/StakeholderService.js
+++ server/services/StakeholderService.js
@@ -5,9 +5,7 @@ const TREETRACKER_STAKEHOLDER_API_URL =
   'http://treetracker-stakeholder-api.stakeholder-api/stakeholders';
 
 const getStakeholderById = async (id) => {
-  const response = await axios.get(
-    `${TREETRACKER_STAKEHOLDER_API_URL}?id=${id}`,
-  );
+  const response = await axios.get(`${TREETRACKER_STAKEHOLDER_API_URL}/${id}`);
 
   return response.data.stakeholders[0];
 };
